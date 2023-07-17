1d8c61fbc070fa4068a9a644582db0a4313e35c2
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-07-17 21:36:57 +0300

    Add files via upload

---
+++ Basic Concepts/User Input/Playing_With_User_Inputs.java
@@ -0,0 +1,18 @@
+// By Alexandros panagiotakopoulos - alexandrospanag.github.io
+
+package com.mycompany.mavenproject1; 
+import javax.swing.*;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+   String firstname,secondname;
+
+      // Get the user's name.
+      firstname = JOptionPane.showInputDialog("Please give the first username? "); //take the first user input
+      secondname = JOptionPane.showInputDialog("Please give the second username? "); //take the second user input
+      // Display message
+      JOptionPane.showMessageDialog(null, "Hello " +firstname+ " and "+ secondname); // print the first message
+      JOptionPane.showMessageDialog(null, "Goodbye " +secondname+ " and "+ firstname); // print the first message
+    }
+}
