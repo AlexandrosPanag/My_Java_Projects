17be3309f26d7d1d27711ff89aea23249f17e77d
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-07-17 21:44:11 +0300

    Add files via upload

---
+++ Basic Concepts/Hello World/HelloWorld.java
@@ -0,0 +1,9 @@
+// By Alexandros panagiotakopoulos - alexandrospanag.github.io
+
+
+public class projectexample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
